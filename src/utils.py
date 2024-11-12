b1b41479cf731206ed1f49db17bb854e29ff050e
janajaber <janajaber02@gmail.com>
2024-11-12 15:50:54 +0100

    ok

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,10 @@
 from openai import OpenAI
-from datasets import load_dataset
 
 def initialize_openai_model():
     client = OpenAI()
     return client
 
-def callGPT(prompt: str, client: OpenAI = OpenAI()):
+def callGPT(prompt: str, client):
     answer = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[{"role": "user", "content": prompt}],
